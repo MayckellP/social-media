63c473e2127a1b3a39f6564d85e59d2e9ce5c6c6
MayckellP <mayckell-perez@hotmail.com>
2024-08-12 16:55:15 +0200

    Log button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -77,130 +77,6 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 }));
 
 export default function Navbar() {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
-    React.useState<null | HTMLElement>(null);
-
-  const isMenuOpen = Boolean(anchorEl);
-  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
-  const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleMobileMenuClose = () => {
-    setMobileMoreAnchorEl(null);
-  };
-
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-    handleMobileMenuClose();
-  };
-
-  const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-    setMobileMoreAnchorEl(event.currentTarget);
-  };
-
-  //*------------------------------------------------------- MENU PROFILE DESKTOP
-  /*   const menuId = "primary-search-account-menu";
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{
-        vertical: "top",
-        horizontal: "right",
-      }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{
-        vertical: "top",
-        horizontal: "right",
-      }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      <MenuItem onClick={handleMenuClose}>
-        <Link href={"/profile/123"} underline="none">
-          <Box>Profile</Box>
-        </Link>
-      </MenuItem>
-      <Link href={"/#"} underline="none">
-        <MenuItem onClick={handleMenuClose}>
-          <ClerkLoaded>
-            <SignedOut>
-              <Link href="/sign-in">Login</Link>
-            </SignedOut>
-          </ClerkLoaded>
-        </MenuItem>
-      </Link>
-    </Menu>
-  );
- */
-  //*------------------------------------------------------- MENU PROFILE MOBILE
-  /*   const mobileMenuId = "primary-search-account-menu-mobile";
-  const renderMobileMenu = (
-    <Menu
-      anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{
-        vertical: "top",
-        horizontal: "right",
-      }}
-      id={mobileMenuId}
-      keepMounted
-      transformOrigin={{
-        vertical: "top",
-        horizontal: "right",
-      }}
-      open={isMobileMenuOpen}
-      onClose={handleMobileMenuClose}
-    >
-      <Link href={"/#"} underline="none">
-        <MenuItem>
-          <IconButton
-            size="large"
-            aria-label="show 4 new mails"
-            color="inherit"
-          >
-            <Badge badgeContent={4} color="error">
-              <MailIcon />
-            </Badge>
-          </IconButton>
-          <p>Messages</p>
-        </MenuItem>
-      </Link>
-      <MenuItem>
-        <IconButton
-          size="large"
-          aria-label="show 17 new notifications"
-          color="inherit"
-        >
-          <Badge badgeContent={17} color="error">
-            <NotificationsIcon />
-          </Badge>
-        </IconButton>
-        <p>Notifications</p>
-      </MenuItem>
-
-      <MenuItem onClick={handleProfileMenuOpen}>
-        <ClerkLoaded>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-        </ClerkLoaded>
-        <IconButton
-          size="large"
-          aria-label="account of current user"
-          aria-controls="primary-search-account-menu"
-          aria-haspopup="true"
-          color="inherit"
-        >
-          <AccountCircle />
-        </IconButton>
-        <p>Profile</p> 
-      </MenuItem>
-    </Menu>
-  ); */
-
   return (
     <Box>
       <AppBar
@@ -218,16 +94,6 @@ export default function Navbar() {
             color: "lightgray",
           }}
         >
-          {/* <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton> */}
-
           {/*------------------------------------------------------- LOGO */}
           <Typography
             /* variant="h5" */
@@ -325,7 +191,12 @@ export default function Navbar() {
             </SearchIconWrapper>
             <StyledInputBase
               placeholder="Search…"
-              inputProps={{ "aria-label": "search" }}
+              inputProps={{
+                "aria-label": "search",
+                sx: {
+                  fontSize: 12,
+                },
+              }}
               sx={{ width: { xs: "100%" } }}
             />
           </Search>
@@ -333,25 +204,13 @@ export default function Navbar() {
           {/* <Box sx={{ flexGrow: 1 }} /> */}
 
           {/*------------------------------------------------------- ICONS */}
-          {/* <ClerkLoaded>
-            <SignedIn>
-              <Box
-                sx={{
-                  backgroundColor: "",
-                  display: { xs: "none", md: "flex" },
-                  width: { xs: "100%", sm: "auto", md: "100px" },
-                  justifyContent: "space-between",
-                }}
-              ></Box>
-            </SignedIn>
-          </ClerkLoaded> */}
 
           {/*------------------------------------------------------- PROFILE */}
           <ClerkLoaded>
             <Box
               sx={{
                 display: {
-                  xs: "none",
+                  xs: "flex",
                   md: "flex",
                 },
               }}
@@ -409,28 +268,8 @@ export default function Navbar() {
               </SignedOut>
             </Box>
           </ClerkLoaded>
-
-          {/*------------------------------------------------------- PROFILE MOBILE SELECT*/}
-          <Box
-            sx={{
-              display: { xs: "flex", md: "none" },
-            }}
-          >
-            <IconButton
-              size="large"
-              aria-label="show more"
-              //aria-controls={mobileMenuId}
-              aria-haspopup="true"
-              onClick={handleMobileMenuOpen}
-              color="inherit"
-            >
-              <MoreIcon />
-            </IconButton>
-          </Box>
         </Toolbar>
       </AppBar>
-      {/* {renderMobileMenu}
-      {renderMenu} */}
     </Box>
   );
 }
